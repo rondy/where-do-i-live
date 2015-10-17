9d078a3b6209dbbb83cca0cdecf4e7faf0dda8d9
Rondy Sousa <rondy.sousa@plataformatec.com.br>
2015-10-17 19:43:31 -0300

    User can consult a generic cep

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -5,6 +5,8 @@ class AddressesController < ApplicationController
   def consult_cep
     if params[:cep] == '01420020'
       @consult_message = 'RUA MARILA, JARDIM PAULISTA. SAO PAULO - SP'
+    elsif params[:cep] == '77500000'
+      @consult_message = 'PORTO NACIONAL - TO'
     else
       @consult_message = 'Não foi possível consultar o CEP (CEP inválido).'
     end
--- spec/acceptance/user_can_consult_a_cep_spec.rb
+++ spec/acceptance/user_can_consult_a_cep_spec.rb
@@ -18,4 +18,13 @@ feature 'User can consult a cep' do
 
     expect(page).to have_content('Não foi possível consultar o CEP (CEP inválido).')
   end
+
+  scenario 'User can consult a generic cep' do
+    visit root_url
+
+    fill_in :cep, with: '77500000'
+    click_button 'Consultar CEP'
+
+    expect(page).to have_content('PORTO NACIONAL - TO')
+  end
 end
