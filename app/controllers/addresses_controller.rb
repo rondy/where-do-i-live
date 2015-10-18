9883486dbb53ed9550a8a801fcac4ae8f8f7f731
Rondy Sousa <rondy.sousa@plataformatec.com.br>
2015-10-18 06:56:17 -0200

    Refactor with perform_cep_consultation

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -3,23 +3,34 @@ class AddressesController < ApplicationController
   end
 
   def consult_cep
-    if params[:cep] == '01420020'
-      @cep_is_valid = true
-      @cep_consultation_message = 'RUA MARILA, JARDIM PAULISTA. SAO PAULO - SP'
-    elsif params[:cep] == '77500000'
-      @cep_is_valid = true
-      @cep_consultation_message = 'PORTO NACIONAL - TO'
-    elsif params[:cep] == '70000000'
-      @cep_is_valid = false
-      @cep_consultation_message = 'Não foi possível consultar o CEP (CEP não registrado).'
-    elsif params[:cep] == '00000000'
-      @cep_is_valid = false
-      @cep_consultation_message = 'Não foi possível consultar o CEP (CEP não informado).'
-    else
-      @cep_is_valid = false
-      @cep_consultation_message = 'Não foi possível consultar o CEP (CEP inválido).'
-    end
+    @cep_consultation = perform_cep_consultation(params[:cep])
 
     render :form
+  end
+
+  private
+
+  def perform_cep_consultation(cep)
+    if cep == '01420020'
+      cep_is_valid = true
+      cep_consultation_message = 'RUA MARILA, JARDIM PAULISTA. SAO PAULO - SP'
+    elsif cep == '77500000'
+      cep_is_valid = true
+      cep_consultation_message = 'PORTO NACIONAL - TO'
+    elsif cep == '70000000'
+      cep_is_valid = false
+      cep_consultation_message = 'Não foi possível consultar o CEP (CEP não registrado).'
+    elsif cep == '00000000'
+      cep_is_valid = false
+      cep_consultation_message = 'Não foi possível consultar o CEP (CEP não informado).'
+    else
+      cep_is_valid = false
+      cep_consultation_message = 'Não foi possível consultar o CEP (CEP inválido).'
+    end
+
+    OpenStruct.new(
+      valid?: cep_is_valid,
+      message: cep_consultation_message,
+    )
   end
 end
