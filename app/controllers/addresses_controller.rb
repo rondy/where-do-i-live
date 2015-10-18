6e4bef51feb4987b5fc905ca08e9eeca4b41430b
Rondy Sousa <rondy.sousa@plataformatec.com.br>
2015-10-18 07:00:59 -0200

    Refactor with CepConsultation

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -11,26 +11,6 @@ class AddressesController < ApplicationController
   private
 
   def perform_cep_consultation(cep)
-    if cep == '01420020'
-      cep_is_valid = true
-      cep_consultation_message = 'RUA MARILA, JARDIM PAULISTA. SAO PAULO - SP'
-    elsif cep == '77500000'
-      cep_is_valid = true
-      cep_consultation_message = 'PORTO NACIONAL - TO'
-    elsif cep == '70000000'
-      cep_is_valid = false
-      cep_consultation_message = 'Não foi possível consultar o CEP (CEP não registrado).'
-    elsif cep == '00000000'
-      cep_is_valid = false
-      cep_consultation_message = 'Não foi possível consultar o CEP (CEP não informado).'
-    else
-      cep_is_valid = false
-      cep_consultation_message = 'Não foi possível consultar o CEP (CEP inválido).'
-    end
-
-    OpenStruct.new(
-      valid?: cep_is_valid,
-      message: cep_consultation_message,
-    )
+    CepConsultation.new.call(cep)
   end
 end
---
+++ spec/models/cep_consultation_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+
+describe CepConsultation do
+  it 'consults a valid cep' do
+    cep = '01420020'
+
+    cep_consultation = CepConsultation.new.call(cep)
+
+    expect(cep_consultation.valid?).to be(true)
+    expect(cep_consultation.message).to eq('RUA MARILA, JARDIM PAULISTA. SAO PAULO - SP')
+  end
+
+  it 'consults a invalid cep' do
+    cep = '775000000'
+
+    cep_consultation = CepConsultation.new.call(cep)
+
+    expect(cep_consultation.valid?).to be(false)
+    expect(cep_consultation.message).to eq('Não foi possível consultar o CEP (CEP inválido).')
+  end
+
+  it 'consults a generic cep' do
+    cep = '77500000'
+
+    cep_consultation = CepConsultation.new.call(cep)
+
+    expect(cep_consultation.valid?).to be(true)
+    expect(cep_consultation.message).to eq('PORTO NACIONAL - TO')
+  end
+
+  it 'consults a not registered cep' do
+    cep = '70000000'
+
+    cep_consultation = CepConsultation.new.call(cep)
+
+    expect(cep_consultation.valid?).to be(false)
+    expect(cep_consultation.message).to eq('Não foi possível consultar o CEP (CEP não registrado).')
+  end
+
+  it 'consults a not informed cep' do
+    cep = '00000000'
+
+    cep_consultation = CepConsultation.new.call(cep)
+
+    expect(cep_consultation.valid?).to be(false)
+    expect(cep_consultation.message).to eq('Não foi possível consultar o CEP (CEP não informado).')
+  end
+end
