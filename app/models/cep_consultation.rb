4077b295706c8cdf42d2b3cc4b1bb9330231b278
Rondy Sousa <rondy.sousa@plataformatec.com.br>
2015-10-18 08:06:41 -0200

    Refactoring with extract method

--- app/models/cep_consultation.rb
+++ app/models/cep_consultation.rb
@@ -2,15 +2,13 @@ class CepConsultation
   def call(cep)
     response = get_cep_response(cep)
 
-    if response[:cod_retorno] == "0"
+    if cep_is_valid?(response)
       cep_is_valid = true
-      part_1 = [response[:ind_tipo_logradouro], response[:nom_bairro]].compact.join(', ')
-      part_2 = [response[:nom_localidade], response[:cod_uf]].compact.join(' - ')
-      cep_consultation_message = [part_1, part_2].reject(&:blank?).join('. ')
-    elsif response[:cod_retorno]=='1' && response[:des_mensagem_amigavel] == "CEP NAO CADASTRADO"
+      cep_consultation_message = build_valid_cep_address(response)
+    elsif cep_is_not_registered?(response)
       cep_is_valid = false
       cep_consultation_message = 'Não foi possível consultar o CEP (CEP não registrado).'
-    elsif response[:cod_retorno]=='1' && response[:des_mensagem_amigavel] == "SPBGE035 - CEP NAO INFORMADO"
+    elsif cep_is_not_informed?(response)
       cep_is_valid = false
       cep_consultation_message = 'Não foi possível consultar o CEP (CEP não informado).'
     else
@@ -25,6 +23,24 @@ class CepConsultation
   end
 
   private
+
+  def cep_is_valid?(response)
+    response[:cod_retorno] == "0"
+  end
+
+  def build_valid_cep_address(response)
+    part_1 = [response[:ind_tipo_logradouro], response[:nom_bairro]].compact.join(', ')
+    part_2 = [response[:nom_localidade], response[:cod_uf]].compact.join(' - ')
+    [part_1, part_2].reject(&:blank?).join('. ')
+  end
+
+  def cep_is_not_registered?(response)
+    response[:cod_retorno] == '1' && response[:des_mensagem_amigavel] == "CEP NAO CADASTRADO"
+  end
+
+  def cep_is_not_informed?(response)
+    response[:cod_retorno] == '1' && response[:des_mensagem_amigavel] == "SPBGE035 - CEP NAO INFORMADO"
+  end
 
   def get_cep_response(cep)
     savon_client = Savon.client do
